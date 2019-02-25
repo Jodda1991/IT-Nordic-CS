94cbfb8ff9821f91447dd9c2b4967a5778232512
Alexander <Jodda1991@gmail.com>
2019-02-25 21:39:01 +0300

    Lesson 2 is added

---
+++ 02/ClassWork/ClassApp/ClassApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите радиус круга:");
+			double radius = double.Parse(Console.ReadLine());
+			double sq = Math.PI * (radius * radius);
+			Console.WriteLine("Площадь круга:" +sq);
+			double x = Math.Pow(2, 10);
+			Console.WriteLine(x);
+
+			char operrand = '+';
+			int num1 = Convert.ToInt32(Console.ReadLine());
+
+			int num2;
+			if(operrand=='+')
+			{
+				Console.WriteLine
+			}
+
+
+
+
+
+			
+		}
+	}
+}
