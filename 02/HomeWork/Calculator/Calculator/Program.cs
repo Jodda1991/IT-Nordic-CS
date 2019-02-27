a091d95ddceea7bd904ef2615a4249d1f2aa97de
Jodda1991 <Jodda1991@gmail.com>
2019-02-27 09:35:58 +0300

    Lesson 2 HomeWork is changed

--- 02/HomeWork/Calculator/Calculator/Program.cs
+++ 02/HomeWork/Calculator/Calculator/Program.cs
@@ -4,53 +4,50 @@ namespace Calculator
 {
 	class Program
 	{
-		static void Main(string[] args)
+		 static void Main(string[] args)
 		{
 
 			while (true)
 			{
 				Console.WriteLine("Enter the first number:");
-				double num1 = Convert.ToDouble(Console.ReadLine());
-				Console.WriteLine("Enter the opperand:");
-				char operrand = Convert.ToChar(Console.ReadLine());
+				double number1 = Convert.ToDouble(Console.ReadLine());
+				Console.WriteLine("Enter the operand:");
+				char operand = Convert.ToChar(Console.ReadLine());
 				Console.WriteLine("Enter the second number:");
-				double num2 = Convert.ToDouble(Console.ReadLine());
+				double number2 = Convert.ToDouble(Console.ReadLine());
+				double result =0;
 
-				if (operrand == '+')
+
+				if (operand == '+')
+				 {
+					result = number1 + number2;
+				 }
+				 else if (operand == '-')
+				 {
+					result = number1 - number2;
+				 }
+				else if (operand == '*')
+				 {
+					result = number1 * number2;
+				 }
+				else if (operand == '/')
+				 {
+					result = number1 / number2;
+				 }
+				else if (operand == '%')
 				{
-					double result1 = num1 + num2;
-					Console.WriteLine("The result is: " + result1);
+					result = number1 % number2;
 				}
-				else if (operrand == '-')
+				else if (operand == '^')
 				{
-					double result2 = num1 - num2;
-					Console.WriteLine("The result is: " + result2);
-				}
-				else if (operrand == '*')
-				{
-					double result3 = num1 * num2;
-					Console.WriteLine("The result is: " + result3);
-				}
-				else if (operrand == '/')
-				{
-					double result4 = num1 / num2;
-					Console.WriteLine("The result is: " + result4);
-				}
-				else if (operrand == '%')
-				{
-					double result5 = num1 % num2;
-					Console.WriteLine("The result is: " + result5);
-				}
-				else if (operrand == '^')
-				{
-					double result6 = Math.Pow(num1, num2);
-					Console.WriteLine("The result is: " + result6);
+					result = Math.Pow(number1, number2);
 				}
 				else
 				{
-
-					Console.WriteLine("Unknown opperand ");
-				}				
+					Console.WriteLine("Unknown operand ");
+				}
+				
+				Console.WriteLine("The result is: " + result);
 			}
 		}
 	}
