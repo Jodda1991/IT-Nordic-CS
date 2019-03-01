e65768e89a19943e2fefe55212529e612c24cc1a
Jodda1991 <Jodda1991@gmail.com>
2019-03-01 21:43:53 +0300

    Lesson 03 HomeWork is added

---
+++ 03/HomeWork/Array/Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] names = new string[3];
+			int[] ages = new int[3];
+
+			for (int i = 0; i < names.Length; i++)
+			{
+				Console.WriteLine("Enter your name:");
+				names[i] = Console.ReadLine();
+			}
+			for (int i = 0; i < ages.Length; i++)
+			{
+				Console.WriteLine("Enter your age:");
+				ages[i] = Convert.ToInt32(Console.ReadLine());
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine($"Name: {names[i]}, age in 4 years: {ages[i] + 4}");
+
+			}
+		}
+	}
+}
