8f5ff5c317cff47879d9f052e6c4a323fefab43b
Alexander <Jodda1991@gmail.com>
2019-03-06 21:25:56 +0300

    Lesson 05 ClasWork added

---
+++ 05/ClassWork/ClassApp1/ClassApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassApp1
+{
+	class Program
+	{
+		[Flags]
+		enum colors
+		{
+			Black,
+			Blue,
+			Cyan,
+			Grey,
+			Green,
+			Magenta,
+			Red,
+			White,
+			Yellow
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Выберите цвета которые будут помещен в избранную политру:");
+			colors[] colors = new colors[4];
+			for(int i =0; i<4;i++)
+			{
+				while (true)
+				{
+					object color;
+					Console.WriteLine("Выберите цвет:");
+					if (Enum.TryParse(typeof(colors), Console.ReadLine(), true, out color))
+					{
+						colors[i] = (colors)color;
+						Console.WriteLine(colors[i]);
+						break;
+
+					}
+					else
+					{
+						Console.WriteLine("Ошибка");
+					}
+					
+				}
+			}
+			if ()
+		}
+	}
+}
---
+++ 05/ClassWork/ClassApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Введите число:");
+				Console.WriteLine(int.Parse(Console.ReadLine()) >= 0
+					? "оно неотрицатльное"
+					: "оно отрицательное");
+			}
+			catch (FormatException exception)
+			{
+				Console.WriteLine("Введены неверные данные!");
+				Console.WriteLine(exception.Message);
+
+			}
+		}
+	}
+}
