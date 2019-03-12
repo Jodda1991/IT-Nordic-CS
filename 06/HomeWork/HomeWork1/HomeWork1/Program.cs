70d73aa847a03124cb8de9318dc488a8831de0ce
Jodda1991 <Jodda1991@gmail.com>
2019-03-12 21:25:50 +0300

    Lesson 06 HomeWork is added

---
+++ 06/HomeWork/HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HomeWork1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a positive integer value:");
+			string Integer;
+			int evenNumber = 0;
+			while(true)
+			{
+				Integer = Console.ReadLine();
+				try
+				{
+					
+					for (int i = 0; i < Integer.Length; i++)
+					{
+						if ((int.Parse(Integer) % 2) == 0)
+						{
+							evenNumber++;
+
+						}
+						while (int.Parse(Integer) <= 0)
+						{
+
+							throw new Exception("You entered a negative number !");
+
+						}
+					}
+					Console.WriteLine($"Total quantity of even numbers is: {evenNumber}");
+					break;
+				}
+				catch (OverflowException)
+				{
+					Console.WriteLine("Your number more then integer max value!");
+				}
+				catch(FormatException)
+				{
+					Console.WriteLine("You entered not a number!");
+				}
+			}
+		}
+	}
+}
---
+++ 06/HomeWork/HomeWork1/HomeWork2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HomeWork2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				double deposit, percentage, expectedDeposit, days = 0;
+				Console.WriteLine("Enter your deposit number in rubbles:");
+				deposit = double.Parse(Console.ReadLine());
+				Console.WriteLine("Enter a daily percentage(1%=0,01):");
+				percentage = double.Parse(Console.ReadLine());
+				Console.WriteLine("Enter an expected deposit:");
+				expectedDeposit = double.Parse(Console.ReadLine());
+				double percent = deposit * percentage;
+				if (deposit <= -1)
+				{
+					throw new Exception("Inccorect! Value have to more then zero !");
+				}
+				if (percentage <= -1)
+				{
+					throw new Exception("Inccorect! Value have to more then zero !");
+				}
+				if (expectedDeposit <= -1)
+				{
+					throw new Exception("Inccorect! Value have to more then zero !");
+				}
+				while (true)
+				{
+					
+					while (deposit < expectedDeposit)
+					{
+
+						deposit = deposit + percent;
+						days++;
+
+					}
+					Console.WriteLine($"You need to wait {days} days");
+					break;
+				}
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("You entered not a number!");
+			}
+		}
+	}
+}
