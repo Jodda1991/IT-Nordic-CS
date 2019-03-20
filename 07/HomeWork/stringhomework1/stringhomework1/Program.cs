8afd476fe60f5e686321da6be9adc7840d10e020
Jodda1991 <Jodda1991@gmail.com>
2019-03-20 11:44:51 +0300

    Lesson 07 part two added

---
+++ 07/HomeWork/stringhomework1/stringhomework1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace stringhomework1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter string with more then 2 words");
+			var enteredWords=string.Empty;
+			int wordsWithA = 0;
+			char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+			string[] words; 
+			while (true)
+			{
+				enteredWords = Console.ReadLine();
+				words = enteredWords.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+				if (words.Length >= 2)
+				{
+					break;
+				}
+				foreach (char digit in digits)
+				{
+					if (enteredWords.Contains(digit))
+					{
+						Console.WriteLine("Error! Unavaible symbols! Try again !");
+						break;
+					}
+				}
+				
+				Console.WriteLine("You entered less then 2 words! Try again !");
+			}			
+			foreach (string word in words)
+			{
+				if (word.StartsWith("A", StringComparison.CurrentCultureIgnoreCase))
+				{
+					wordsWithA++;
+				}
+			}
+			
+			Console.WriteLine($"Words start with A are {wordsWithA}");
+		}
+	}
+}
--- 08/HomeWork/HomeWorkApp/HomeWorkApp/Program.cs
+++ 08/HomeWork/HomeWorkApp/HomeWorkApp/Program.cs
@@ -8,19 +8,20 @@ namespace HomeWorkApp
 	{
 		static void Main(string[] args)
 		{
-			var signs = new Dictionary<string, string>();
-			signs.Add("(",")");
-			signs.Add("[","]");
+			var signs = new Dictionary<char, char>();
+			signs.Add('(',')');
+			signs.Add('[',']');
 			Console.WriteLine("Please enter signs. Possible signs are []or()");
 			string input = Console.ReadLine();
-			KeyValuePair<string, string> elements = signs.ElementAt(0);
-			string start = elements.Key;
-			string exit = elements.Value;
-			KeyValuePair<string, string> elements1 = signs.ElementAt(1);
-			string start1 = elements1.Key;
-			string exit1 = elements1.Value;
+			KeyValuePair<char, char> elements = signs.ElementAt(0);
+			char start = elements.Key;
+			char end = elements.Value;
+			KeyValuePair<char, char> elements1 = signs.ElementAt(1);
+			char start1 = elements1.Key;
+			char end1 = elements1.Value;
+			int count = 0;
 
-			if ((input.StartsWith(start)&&input.EndsWith(exit))||(input.StartsWith(start1)&&input.EndsWith(exit1)))
+			if ((input.StartsWith(start)&&input.EndsWith(end))||(input.StartsWith(start1)&&input.EndsWith(end1)))
 			{
 				Console.WriteLine("Correct!");
 			}
