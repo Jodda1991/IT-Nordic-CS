0b694aca39a4ecf00d2fe8e97519bb70c6f0f5f2
Alexander <Jodda1991@gmail.com>
2019-03-27 21:28:36 +0300

    Lesson 11 ClassWork

---
+++ 11/ClassWork/ConsoleApp1/ConsoleApp1/Pet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class Pet
+	{
+		public string Kind;
+		public string Name;
+		public char Sex;
+		public DateTimeOffset DateOfBirth ;
+
+		public int Age
+		{
+			get {
+				TimeSpan age = DateTimeOffset.UtcNow.Subtract(DateOfBirth);
+				return Convert.ToInt32(Math.Floor(age.TotalDays / 365.242));
+				}
+		}
+
+		public string ShortDescription
+		{
+			get
+			{
+				return $"{Name} is a {Kind}";
+			}
+		}
+
+		public void WriteDescriptions()
+		{	
+				bool showFullDescriptions = true;
+
+				Console.WriteLine(showFullDescriptions
+						 ?PropertiesString
+						 :ShortDescription);
+		}
+
+
+		public string PropertiesString
+		{
+			get
+			{
+				return $"{Name} is a {Kind} ({Sex}) of {Age} years old.";
+			}
+		}
+	}
+}
---
+++ 11/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet dog = new Pet();
+			dog.Kind = "dog";
+			dog.Name = "Spotty";
+			dog.Sex = 'M';
+			dog.DateOfBirth = DateTimeOffset.Parse("2017-02-24");
+
+			dog.WriteDescriptions(false);
+		}
+	}
+}
