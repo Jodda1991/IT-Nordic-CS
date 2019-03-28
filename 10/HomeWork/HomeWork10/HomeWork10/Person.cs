d02543feb76004dce9b1eb9a4838b96fc79e9f8a
Jodda1991 <Jodda1991@gmail.com>
2019-03-28 09:23:39 +0300

    Lesson 10 HomeWork is added

---
+++ 10/HomeWork/HomeWork10/HomeWork10/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork10
+{
+	class Person
+	{
+		public string Name { get; set; }
+		public int  Age  { get; set; }
+
+	public int AgesInFourYears
+		{
+			get
+			{
+				return (Age + 4);
+			}
+		}
+		public string Output
+		{
+			get{ return $"Name: {Name} ,in 4 years: {AgesInFourYears}";}
+		}
+	}
+}
---
+++ 10/HomeWork/HomeWork10/HomeWork10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person[] people = new Person[3];
+			for (int i = 0; i<3;i++)
+			{
+				people[i] = new Person();
+				Console.WriteLine("Enter person's name:");
+				people[i].Name = Console.ReadLine();
+				Console.WriteLine("Enter person's age:");
+				people[i].Age = int.Parse(Console.ReadLine());
+			}
+			for (int i=0; i < 3; i++)
+			{
+				Console.WriteLine(people[i].Output);
+			}
+		}
+	}
+}
