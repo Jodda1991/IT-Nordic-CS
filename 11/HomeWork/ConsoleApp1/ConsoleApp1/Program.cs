f17e3a7e37255e093b498954327dc2beee86b900
Jodda1991 <Jodda1991@gmail.com>
2019-03-28 18:05:32 +0300

    Lesson 11 HomeWork is added

---
+++ 11/HomeWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ReminderItem reminder = new ReminderItem();
+			reminder.AlarmDate = DateTimeOffset.Parse("2019-04-02 12:30");
+			reminder.AlarmMessage = "Important meeting";
+			reminder.WriteProperties();
+
+			ReminderItem reminder1 = new ReminderItem();
+			reminder1.AlarmDate = DateTimeOffset.Parse("2019-03-27 19:40");
+			reminder1.AlarmMessage = "Don't forget potatoes in the oven!";
+			reminder1.WriteProperties();
+		}
+	}
+}
---
+++ 11/HomeWork/ConsoleApp1/ConsoleApp1/ReminderItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class ReminderItem
+	{
+		public DateTimeOffset AlarmDate { get; set; }
+
+		public string AlarmMessage { get; set; }
+
+		public int TimeToAlarm
+		{
+			get { TimeSpan timeForSleep = AlarmDate -DateTimeOffset.UtcNow;
+				return Convert.ToInt32(timeForSleep.TotalMinutes); }
+		}
+
+		public bool IsOutdated
+		{
+			get {
+				bool youHaveATime=true;
+				if (TimeToAlarm>=0)
+				{
+					youHaveATime = true;
+				}
+				else if(TimeToAlarm<0)
+				{
+					youHaveATime = false;
+				}
+				return youHaveATime;
+			}
+		}
+
+		public void WriteProperties()
+		{
+			string inTimeOrLate;
+			if(IsOutdated)
+			{
+				inTimeOrLate = "you have some time";
+			}
+			else
+			{
+				inTimeOrLate = "you have no time!";
+			}
+
+			Console.WriteLine($"Your alarm was set on {AlarmDate}\n with message: {AlarmMessage}\n and {inTimeOrLate} : ({TimeToAlarm} minutes)");
+		}
+
+
+
+
+
+
+	}
+}
