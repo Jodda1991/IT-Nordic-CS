9fab7ce5fd12f3bb98e548fe1b1c7ed50f5036b0
Jodda1991 <Jodda1991@gmail.com>
2019-03-28 18:13:35 +0300

    Lesson 10 homeWork is added

--- 10/HomeWork/HomeWork10/HomeWork10/Program.cs
+++ 10/HomeWork/HomeWork10/HomeWork10/Program.cs
@@ -7,6 +7,7 @@ namespace HomeWork10
 		static void Main(string[] args)
 		{
 			Person[] people = new Person[3];
+
 			for (int i = 0; i<3;i++)
 			{
 				people[i] = new Person();
@@ -15,6 +16,7 @@ namespace HomeWork10
 				Console.WriteLine("Enter person's age:");
 				people[i].Age = int.Parse(Console.ReadLine());
 			}
+
 			for (int i=0; i < 3; i++)
 			{
 				Console.WriteLine(people[i].Output);
