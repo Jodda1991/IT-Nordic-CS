b6a07866e1fb0957c4c11d6f3fd21f1d8dfca9da
Jodda1991 <Jodda1991@gmail.com>
2019-03-31 11:53:46 +0300

    Lesson 05 Homework is updated

--- 05/HomeWork/Colors/Program.cs
+++ 05/HomeWork/Colors/Program.cs
@@ -10,7 +10,6 @@ namespace Colors
 		[Flags]
 		enum Colors
 		{
-			None=0,
 			Black=1,
 			Blue=2,
 			Cyan=4,
@@ -23,21 +22,14 @@ namespace Colors
 		}
 		static void Main(string[] args)
 		{
-			var array = Enum.GetValues(typeof(Colors));
 			Colors allColors = Colors.Black | Colors.Blue | Colors.Cyan
 				| Colors.Grey | Colors.Green | Colors.Magenta
 				| Colors.Red | Colors.White | Colors.Yellow;
-			var all = (Colors.Black | Colors.Blue | Colors.Cyan
-				| Colors.Grey | Colors.Green | Colors.Magenta
-				| Colors.Red | Colors.White | Colors.Yellow).ToString();
 
 			Console.WriteLine(
 				"All colors: {0}",
 				string.Join(' ', Enum.GetNames(typeof(Colors))));
-			Console.WriteLine("Choose colors as favorite:");
-			Colors[] favorite = new Colors[4];
-			Colors chosenColors = Colors.None;
-			var favoriteColors=chosenColors;
+			Colors chosenColors = new Colors();
 			var chosen=string.Empty;
 
 			for (int i = 0; i < 4; i++)
@@ -45,12 +37,11 @@ namespace Colors
 				object color;
 				while (true)
 				{					
-					Console.WriteLine("Choose color:");
+					Console.WriteLine("Choose colors as favorite:");
 					if (Enum.TryParse(typeof(Colors), Console.ReadLine(), true, out color))
 					{
-						favorite[i] = (Colors)color;
-						favoriteColors = favorite[i];
-						chosen = favoriteColors.ToString();						
+						chosenColors = chosenColors|(Colors)color;
+						chosen = chosenColors.ToString();						
 						break;
 					}
 					else
@@ -58,11 +49,10 @@ namespace Colors
 						Console.WriteLine("Error");
 					}
 				}
-				chosenColors = chosenColors | favoriteColors;
 			}
 			
-			var otherColors = (allColors ^ favoriteColors).ToString();	
-			Console.WriteLine("You favorite color is :" + chosenColors.ToString());
+			var otherColors = (allColors^chosenColors).ToString();	
+			Console.WriteLine("You favorite colors is :" + chosen);
 			Console.WriteLine("Other colors are : " + otherColors);
 		}			
 	}
