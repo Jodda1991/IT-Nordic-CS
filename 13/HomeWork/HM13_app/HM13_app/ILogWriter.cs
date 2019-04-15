feefe24b636d8cc1cc4a97222c84e6e0159c99b5
Jodda1991 <Jodda1991@gmail.com>
2019-04-15 10:33:33 +0300

    Lesson 13 HomeWork

---
+++ 13/HomeWork/HM13_app/HM13_app/ILogWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HM13_app
+{
+	public interface ILogWriter
+	{
+		void LogInfo(string message);
+
+		void LogWarning(string message);
+
+		void LogError(string message);
+	}
+}
