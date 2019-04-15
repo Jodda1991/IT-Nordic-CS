7865866e013d05b525ac30a4a5e891efd4c198f7
Alexander <Jodda1991@gmail.com>
2019-04-15 21:26:04 +0300

    ClassWork 16 is added

---
+++ 16/ClassWork/CW 16/CW 16/Calculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CW_16
+{
+	class Calculation
+	{
+		int[] _array;
+
+		public Calculation(int[]array)
+		{
+			_array = array;
+		}
+
+		public int CalcSingleValue(Func<int[],int>func)
+		{
+			return func(_array);
+		}
+	}
+
+}
---
+++ 16/ClassWork/CW 16/CW 16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CW_16
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const double unchangeble = 1.8; 
+			var circle = new Circle(unchangeble);
+
+			var square = circle.Calculate(
+				(double r) => Math.PI* Math.Pow(unchangeble, 2)
+			);
+
+			Console.WriteLine(square);
+
+			Calculation s = new Calculation(new[] { 1, 4, 12,48 });
+			var y=s.CalcSingleValue((int[] array) =>
+			{
+				int arr = 0;
+				foreach (int z in array)
+				{
+					arr =+ z;
+				}
+				return arr / array.Length;
+			});
+			Console.WriteLine(y);
+		}
+
+	}
+}
