78a0f3ccf8759b7da5e23b21f650dd5897fd9a41
Jodda1991 <Jodda1991@gmail.com>
2019-04-21 21:09:46 +0300

    Lesson 14 - not yet

---
+++ 14/HomeWork/HM13_app/HM13_app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HM14_app
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ConsoleLogWriter demo =ConsoleLogWriter.GetSingle();
+
+			var fileLogger = new FileLogWriter();
+
+			using (var logger = new MultipleLogWriter(demo, fileLogger))
+			{
+				logger.LogInfo("Test information message");
+				logger.LogWarning("Test warning message");
+				logger.LogError("Test error message");
+			}
+		}
+	}
+}
