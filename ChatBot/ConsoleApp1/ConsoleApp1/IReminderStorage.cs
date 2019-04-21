6237fa64a94ca1554a7bd53dfab9a84456a3480e
Jodda1991 <Jodda1991@gmail.com>
2019-04-21 21:08:48 +0300

    ChatBot first step

---
+++ ChatBot/ConsoleApp1/ConsoleApp1/IReminderStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	interface IReminderStorage
+	{
+		void Add();
+
+		void Update();
+
+		void Get();
+	}
+}
---
+++ ChatBot/ConsoleApp1/ConsoleApp1/ReminderItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class ReminderItem: IReminderStorage,IReminderSender
+	{
+		public DateTimeOffset AlarmDate { get; set; }
+
+		public string AlarmMessage { get; set; }
+
+		public string ContactId { get; set; }
+
+		Guid Id
+		{
+			get { return Guid.NewGuid(); }
+		}
+
+		public TimeSpan TimeToAlarm
+		{
+			get { return AlarmDate.Subtract(DateTimeOffset.UtcNow); }
+		}
+
+		public bool IsOutdated
+		{
+			get
+			{
+				return TimeToAlarm >= TimeSpan.Zero
+				   ? true
+				   : false;
+			}
+		}
+
+		public ReminderItem(DateTimeOffset alarmDate, string alarmMessage, string contactId)
+		{
+			AlarmDate = alarmDate;
+			AlarmMessage = alarmMessage;
+			ContactId = contactId;	
+		}
+
+		public void Add()
+		{
+			
+		}
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Get()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ReminderItem[] GetReminderItemsToSend()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
