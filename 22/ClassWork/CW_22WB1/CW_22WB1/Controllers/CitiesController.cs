828a50c87ada2395cc334ffb21242b4a92618ddd
Alexander <Jodda1991@gmail.com>
2019-05-08 21:35:12 +0300

    Lesson 22 ClassWork

---
+++ 22/ClassWork/CW_22WB1/CW_22WB1/Controllers/CitiesController.cs
@@ -0,0 +1,74 @@
+﻿using CW_22WB1.DataStore;
+using CW_22WB1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CW_22WB1.Controllers
+{
+	[Route("/api/cities")]
+	public class CitiesController:Controller
+	{
+		[HttpGet()]
+		public JsonResult GetCities()
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			var cities = citiesDataStore.Cities;
+			return new JsonResult(cities);
+			/*return new JsonResult(
+				new List<City>
+				{
+					new City
+					{
+						Id=1,
+						Name = "Moscow",
+						Description = "The capital of Russia"
+					},
+
+					new City
+					{
+						Id=2,
+						Name = "New York",
+						Description = "The one of the biggest cities in the world"
+					},
+				});
+				*/
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetCity(int id)
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			var city = citiesDataStore.Cities
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
+
+			if (city == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(city);
+		}
+		[HttpPost()]
+		public IActionResult CreateCity([FromBody]CityCreateModel city)
+		{
+			if(city ==null)
+			{
+				BadRequest();
+			}
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			int newCityId = citiesDataStore.Cities.Max(c => c.Id) + 1;
+
+			var newCity = new CityGetModel
+			{
+				Id = newCityId,
+				Name = city.Name,
+				Description = city.Description,
+				NumberOfPointsOfInterest = city.NumberOfPointsOfInterest
+			};
+			citiesDataStore.Cities.Add(newCity);
+
+			return Ok();
+		}
+	}
+}
---
+++ 22/ClassWork/CW_22WB1/CW_22WB1/DataStore/CitiesDataStore.cs
@@ -0,0 +1,45 @@
+﻿using CW_22WB1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW_22WB1.DataStore
+{
+	public class CitiesDataStore
+	{
+		private static CitiesDataStore _store;
+
+		public List<CityGetModel> Cities {get;private set;}
+
+		public static CitiesDataStore GetInstance()
+		{
+			if (_store == null)
+			{
+				_store = new CitiesDataStore();
+			}
+			return _store;
+		}
+
+		private CitiesDataStore()
+		{
+			Cities = new List<CityGetModel>
+				{					
+					new CityGetModel
+					{
+						Id=1,
+						Name = "Moscow",
+						Description = "The capital of Russia"
+					},
+
+					new CityGetModel
+					{
+						Id=2,
+						Name = "New York",
+						Description = "The one of the biggest cities in the world"
+					},
+				};
+				
+		}
+	}
+}
