0b6f67c6bbcd4cccf4805582c4a88446036eab26
Jodda1991 <Jodda1991@gmail.com>
2019-05-12 14:56:52 +0300

    Lesson 24 HomeWork is added

---
+++ 22/HomeWork/CW_22WB1/Controllers/CitiesController.cs
@@ -0,0 +1,127 @@
+﻿using CW_22WB1.DataStore;
+using CW_22WB1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CW_22WB1.Controllers
+{
+	[Route("/api/cities")]
+	public class CitiesController:Controller
+	{
+		[HttpGet()]
+		public JsonResult GetCities()
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			var cities = citiesDataStore.Cities;
+			return new JsonResult(cities);
+			/*return new JsonResult(
+				new List<City>
+				{
+					new City
+					{
+						Id=1,
+						Name = "Moscow",
+						Description = "The capital of Russia"
+					},
+
+					new City
+					{
+						Id=2,
+						Name = "New York",
+						Description = "The one of the biggest cities in the world"
+					},
+				});
+				*/
+		}
+		[HttpGet("{id}",Name = "GetCity")]
+		public IActionResult GetCity(int id)
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			var city = citiesDataStore.Cities
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
+
+			if (city == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(city);
+		}
+		[HttpPost()]
+		public IActionResult CreateCity([FromBody]CityCreateModel city)
+		{
+			if(city ==null)
+			{
+				BadRequest();
+			}
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			int newCityId = citiesDataStore.Cities.Max(c => c.Id) + 1;
+
+			var newCity = new CityGetModel
+			{
+				Id = newCityId,
+				Name = city.Name,
+				Description = city.Description,
+				NumberOfPointsOfInterest = city.NumberOfPointsOfInterest
+			};
+			citiesDataStore.Cities.Add(newCity);
+
+			return CreatedAtRoute(
+				"GetCity",new {id = newCityId },
+				newCity);
+		}
+
+		[HttpPut("{id}")]
+		public  IActionResult UpdateCity(int id, [FromBody] CityCreateModel city)
+		{
+			if (city == null)
+			{
+				BadRequest();
+			}
+
+			var citiesDataStore = CitiesDataStore.GetInstance();
+
+			var updatedCity = citiesDataStore.Cities
+					.Where(x => x.Id == id)
+					.FirstOrDefault();
+			if(updatedCity==null)
+			{
+				NotFound();
+			}
+			if (updatedCity != null)
+			{
+				updatedCity.Name = updatedCity.Name;
+				updatedCity.Description = updatedCity.Description;
+				updatedCity.NumberOfPointsOfInterest = updatedCity.NumberOfPointsOfInterest;
+
+			}
+
+			return Ok(city);
+
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult RemoveCity(int id)
+		{
+
+			var citiesDataStore = CitiesDataStore.GetInstance();
+
+			var removedCity = citiesDataStore.Cities
+					.Where(x => x.Id == id)
+					.FirstOrDefault();
+
+			if (removedCity == null)
+			{
+				NotFound();
+			}
+
+			citiesDataStore.Cities.Remove(removedCity);
+
+			return Ok();
+
+		}
+
+	}
+}
