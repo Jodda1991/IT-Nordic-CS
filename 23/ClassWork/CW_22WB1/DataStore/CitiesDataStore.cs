e24311a6d1d2516886356a3df5028c3a6cec13fb
Alexander <Jodda1991@gmail.com>
2019-05-13 21:36:02 +0300

    Lesson 23 ClassWork

---
+++ 23/ClassWork/CW_22WB1/DataStore/CitiesDataStore.cs
@@ -0,0 +1,36 @@
+﻿using CW_23WB1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW_23WB1.DataStore
+{
+	public class CitiesDataStore:ICitiesDataStore
+	{
+		
+		public List<CityGetModel> Cities {get;private set;}
+
+
+		public CitiesDataStore()
+		{
+			Cities = new List<CityGetModel>
+				{					
+					new CityGetModel
+					{
+						Id=1,
+						Name = "Moscow",
+						Description = "The capital of Russia"
+					},
+
+					new CityGetModel
+					{
+						Id=2,
+						Name = "New York",
+						Description = "The one of the biggest cities in the world"
+					},
+				};
+				
+		}
+	}
+}
---
+++ 23/ClassWork/CW_22WB1/DataStore/ICitiesDataStore.cs
@@ -0,0 +1,13 @@
+﻿using CW_23WB1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW_23WB1.DataStore
+{
+	public interface ICitiesDataStore
+	{
+		List<CityGetModel> Cities { get; }
+	}
+}
